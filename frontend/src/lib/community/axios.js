6f9ff0b9074d6a98bfb3636adab80e2baf14cbfe
Gazi Momin Iqbal <gazimiqbal@gmail.com>
2025-05-11 22:33:54 -0700

    update axios baseurl 2

--- frontend/src/lib/community/axios.js
+++ frontend/src/lib/community/axios.js
@@ -5,6 +5,6 @@ axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 
 export const axiosInstance = axios.create({
   // baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api/community" : "/api/community",
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api/community" : import.meta.env.VITE_BACKEND_URL,
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api/community" : import.meta.env.VITE_BACKEND_URL + "/api/community",
   withCredentials: true,
 });
--- frontend/src/lib/tinder/axios.js
+++ frontend/src/lib/tinder/axios.js
@@ -4,7 +4,7 @@ axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 
 
 // const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/tinder" : "/api/tinder";
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/tinder" : import.meta.env.VITE_BACKEND_URL;
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/tinder" : import.meta.env.VITE_BACKEND_URL + '/api/tinder';
 
 
 export const axiosInstance = axios.create({
