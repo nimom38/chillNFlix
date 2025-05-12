d08906a33ec242c65f3ea547f1f70363d05c6c49
Gazi Momin Iqbal <gazimiqbal@gmail.com>
2025-05-11 22:40:44 -0700

    update socket baseurl

--- frontend/src/socket/tinder/socket.client.js
+++ frontend/src/socket/tinder/socket.client.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
+const SOCKET_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : import.meta.env.VITE_BACKEND_URL;
 
 let socket = null;
 
